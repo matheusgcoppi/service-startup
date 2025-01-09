f56de3d6c60bd3ab40c37175518678bf1ef78afc
matheus coppi <matheus.coppi@gruppe.com.br>
2025-01-09 18:52:11 -0300

    Add api and foundation

--- api/service/sales/main.go
+++ api/service/sales/main.go
@@ -1,11 +1,30 @@
-package sales
+package main
 
 import (
 	"context"
+	"github.com/matheusgcoppi/service/foundation/logger"
 	"os"
+	"os/signal"
+	"runtime"
+	"syscall"
 )
 
 func main() {
+	var log *logger.Logger
+
+	events := logger.Events{
+		Error: func(ctx context.Context, r logger.Record) {
+			log.Info(ctx, "****** SEND ALERT ******")
+		},
+	}
+
+	traceIDFn := func(ctx context.Context) string {
+		return "" //web.GetTraceID(ctx)
+	}
+
+	log = logger.NewWithEvents(os.Stdout, logger.LevelInfo, "SALES", traceIDFn, events)
+
+	// ---------------------------------------------------------------------
 	ctx := context.Background()
 
 	if err := run(ctx, log); err != nil {
@@ -15,6 +34,18 @@ func main() {
 
 }
 
-func run(ctx context.Context log *logger.Logger) error {
+func run(ctx context.Context, log *logger.Logger) error {
+	// ----------------------------------------------------------------------
+	// GOMAXPROCS
 
+	log.Info(ctx, "startup", "GOMAXPROCS", runtime.GOMAXPROCS(0))
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
+	sig := <-shutdown
+
+	log.Info(ctx, "shutdown", "status", "shutdown started", "signal", sig)
+	defer log.Info(ctx, "shutdown", "status", "shutdown completed", "signal", sig)
+
+	return nil
 }
