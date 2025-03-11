1ec67b71383989a4504f3fc5bdb807c0c24a07ef
Matheus <matheuscoppi22@gmail.com>
2025-03-10 21:24:22 -0300

    Add error handling

---
+++ api/services/api/mid/errors.go
@@ -0,0 +1,40 @@
+package mid
+
+import (
+	"context"
+	"errors"
+	"github.com/matheusgcoppi/service/app/api/mid"
+	"github.com/matheusgcoppi/service/app/sdk/errs"
+	"github.com/matheusgcoppi/service/foundation/logger"
+	"github.com/matheusgcoppi/service/foundation/web"
+	"net/http"
+)
+
+// Errors executed the errors middleware functionality.
+func Errors(log *logger.Logger) web.MidHandler {
+	m := func(next web.Handler) web.Handler {
+		h := func(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+			hdl := func(ctx context.Context) error {
+				return next(ctx, w, r)
+			}
+
+			if err := mid.Errors(ctx, log, hdl); err != nil {
+				var currentError errs.Error
+				errors.As(err, &currentError)
+				if err := web.Respond(ctx, w, currentError, currentError.Code.HttpStatus()); err != nil {
+					return err
+				}
+
+				// If we receive the shutdown err we need to return it
+				// to the base handler to shut down the service.
+				if web.IsShutdown(err) {
+					return err
+				}
+			}
+			return nil
+		}
+
+		return h
+	}
+	return m
+}
